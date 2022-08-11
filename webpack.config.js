bdd424e5ddad6b5fb533f3e72b57689da653f210
besated <bryan@falconwallet.app>
2022-08-11 16:45:19 -0700

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,26 @@
 const path = require('path');
 
 module.exports = {
-    mode: process.env.NODE_ENV || 'development',
-    devtool: 'inline-source-map',
-    bail: true,
-    entry: {
-        injection: path.join(__dirname, 'src/injection.ts'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx|tsx|ts)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    }
+  mode: process.env.NODE_ENV || 'development',
+  devtool: 'inline-source-map',
+  bail: true,
+  entry: {
+    injection: path.join(__dirname, 'src/injection.ts'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
 };
